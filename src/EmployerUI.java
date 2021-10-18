b0040de52a606c69743c07a1525e7549edd76951
wihobbs <hobbsman58@gmail.com>
2021-10-18 08:00:10 -0400

    added user interfaces

---
+++ src/EmployerUI.java
@@ -0,0 +1,39 @@
+/**
+ * Employer User Interface
+ * Runs the front-end of the employer's experience with the Internship program.
+ * @author William Hobbs
+ */
+public class EmployerUI {
+    /**
+     * addProfile
+     * Helps an employer set up their Employer Profile for students to search.
+     */
+    private void addProfile() {
+
+    }
+
+    /**
+     * addListing
+     * Employer adds a new job listing for students to apply.
+     */
+    private void addListing() {
+
+    }
+
+    /**
+     * deleteListing
+     * Employer can delete their job listings
+     */
+    private void deleteListing() {
+
+    }
+
+    /**
+     * editListing
+     * Employer can edit their job listings
+     */
+    private void editListing() {
+
+    }
+
+}
