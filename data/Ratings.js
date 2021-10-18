c4eddbb32aac1e648b740f72d03a97f009688552
Jack <jackcoberman@gmail.com>
2021-10-17 23:13:41 -0400

    Added two ratings

---
+++ data/Ratings.js
@@ -0,0 +1,16 @@
+let ratings = [
+    {
+        "id": "1005fe40-dcd8-4e84-b0fc-810f25768c5c",
+        "receiverId": "P1289123",
+        "rating": 2,
+        "comment": "With no skills, this kid can't even teach me how to dougie",
+        "writerId": "01f78a8e-21a0-40cc-8b03-6155cb92986f"
+    },
+    {
+        "id": "b896d79d-0528-41be-850d-7f684db13d5a",
+        "receiverId": "B8710298",
+        "rating": 5,
+        "comment": "This genius could get help me master the dougie AND the nay-nay",
+        "writerId": "01f78a8e-21a0-40cc-8b03-6155cb92986f"
+    }  
+];
