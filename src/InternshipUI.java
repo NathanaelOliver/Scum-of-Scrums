4cf8ecce65987feff746da68d7051d7b3f4e264c
Jack <jackcoberman@gmail.com>
2021-10-21 13:51:18 -0400

    Added implementation to internshipUI

--- src/InternshipUI.java
+++ src/InternshipUI.java
@@ -3,35 +3,36 @@ package src;
 /**
  * Intership User Interface
  * This user interface runs the front-end of the Internship system.
- * @author William Hobbs
+ * @author William Hobbs and Jack Oberman
  */
 
 import java.util.Scanner;
 
-public class InternshipUI {
-    private User currentUser;
-    private Scanner scanner;
-    private String[] mainMenuOptions;
+public abstract class InternshipUI {
+    protected User currentUser;
+    protected Scanner scanner;
+    protected String[] mainMenuOptions;
 
     /**
      * Runs the internship user interface
      */
-    public void run() {
-
-    }
+    public abstract void run();
 
     /**
      * Logs a user out of the system and clears data
      */
-    private void logout() {
-
+    protected void logout() {
+        //TO-DO: DataWriter.save();
     }
 
     /**
      * Displays main menu options for the user
      */
-    private void displayMenu() {
-
-    }
+    protected void displayMainMenu() {
+        for (int i = 0; i < this.mainMenuOptions.length; ++i) {
+            System.out.println((i+1) + ") " + this.mainMenuOptions[i]);
+        }
+        System.out.println("Pick 1 through " + this.mainMenuOptions.length);
+    };
 
 }
