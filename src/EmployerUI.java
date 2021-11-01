20b98d7a0bc596c5e0c46c545dc86718c2562a2d
wihobbs <hobbsman58@gmail.com>
2021-10-31 21:17:39 -0400

    fixed other infinite loop bug

--- src/EmployerUI.java
+++ src/EmployerUI.java
@@ -151,32 +151,36 @@ public class EmployerUI extends InternshipUI {
      * editListing Employer can edit their job listings
      */
     private void editListing() {
-        flush();
-        for (int i = 0; i < this.employer.getListings().size(); i++) {
-            System.out.println((i+1) + ". " + this.employer.getListings().get(i).getTitle());
-        }
-        int input = readInt("Which listing would you like to edit?", 1, (this.employer.getListings().size()));
-        Listing listing = this.employer.getListings().get(input - 1);
-        System.out.println("What would you like to edit?");
-        int editing = readMenu(this.editOptions);
-        switch (editing) { 
-            case 1: listing.setPayRate(readDouble("Please give the pay rate of the listing as an hourly wage"));
-                    break;
-            case 2: listing.setSkills(readSkills(listing.getSkills()));
-                    break;
-            case 3: listing.setStartDate(readDate("Enter the new start date."));
-                    break;
-            case 4: listing.setStartDate(readDate("Enter the new end date."));
-                    break;
-            case 5: listing.setSiteLink(readWord("Enter the new site link."));
-                    break;
-            case 6: listing.setTitle(readString("Enter the new title."));
-                    break;
-            case 7: listing.setLocation(readString("Enter the new location."));
-                    break;
-            case 8: listing.setDescription((readStringArrayList("Please add another line of the job description.", 
-                        "Would you like to add another line of the job description?")));
-                    break;
+        if (this.employer.getListings().size() > 0) {
+            flush();
+            for (int i = 0; i < this.employer.getListings().size(); i++) {
+                System.out.println((i+1) + ". " + this.employer.getListings().get(i).getTitle());
+            }
+            int input = readInt("Which listing would you like to edit?", 1, (this.employer.getListings().size()));
+            Listing listing = this.employer.getListings().get(input - 1);
+            System.out.println("What would you like to edit?");
+            int editing = readMenu(this.editOptions);
+            switch (editing) { 
+                case 1: listing.setPayRate(readDouble("Please give the pay rate of the listing as an hourly wage"));
+                        break;
+                case 2: listing.setSkills(readSkills(listing.getSkills()));
+                        break;
+                case 3: listing.setStartDate(readDate("Enter the new start date."));
+                        break;
+                case 4: listing.setStartDate(readDate("Enter the new end date."));
+                        break;
+                case 5: listing.setSiteLink(readWord("Enter the new site link."));
+                        break;
+                case 6: listing.setTitle(readString("Enter the new title."));
+                        break;
+                case 7: listing.setLocation(readString("Enter the new location."));
+                        break;
+                case 8: listing.setDescription((readStringArrayList("Please add another line of the job description.", 
+                            "Would you like to add another line of the job description?")));
+                        break;
+            }
+        } else {
+        System.out.println("There are no listings to view.");
         }
     }
 } 
