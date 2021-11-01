a9ffadffda29ccd102be2c266cdfff0ae5fae22d
Jack <jackcoberman@gmail.com>
2021-10-31 23:45:08 -0400

    Added colons

--- src/Resume.java
+++ src/Resume.java
@@ -339,19 +339,19 @@ public class Resume implements JSONable {
         }
         ret += "\nEducation:\n * University of South Carolina\n * Bachelors in Computer Information Systems\n * Expected Graduation: " + this.year;
         if (this.workExperiences.size() > 0) {
-            ret += "\n\nWork Experiences\n";
+            ret += "\n\nWork Experiences:\n";
             for (int i = 0; i < (this.workExperiences.size()); i++) {
                 ret += this.workExperiences.get(i).toString() + "\n";
             }
         }
         if (this.clubExperiences.size() > 0) {
-            ret += "\nClub Experiences\n";
+            ret += "\nClub Experiences:\n";
             for (int i = 0; i < (this.clubExperiences.size()); i++) {
                 ret += this.clubExperiences.get(i).toString() + "\n";
             }
         }
         if (this.courseExperiences.size() > 0) {
-            ret += "\nCourse Experiences\n";
+            ret += "\nCourse Experiences:\n";
             for (int i = 0; i < (this.courseExperiences.size()); i++) {
                 ret += this.courseExperiences.get(i).toString() + "\n";
             }
