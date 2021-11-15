0df5b66ba023b26faaa8e04f2413f461fd252aab
wihobbs <hobbsman58@gmail.com>
2021-11-14 20:24:02 -0500

    Club Experience tester

---
+++ test/ClubExperienceTest.java
@@ -0,0 +1,83 @@
+package test;
+
+import src.Database;
+import src.ClubExperience;
+import src.DataLoader;
+import src.Date;
+
+import java.util.UUID;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ClubExperienceTest {
+	private ClubExperience experience;
+    private ClubExperience experience2;
+	
+	@BeforeClass
+	public static void oneTimeSetup() {
+        
+	}
+	
+	@AfterClass
+	public static void oneTimeTearDown() {
+		
+	}
+	
+	@BeforeEach
+	public void setup() {
+        Database.fromJSON(DataLoader.getJSON("data/Users.json"));
+        ArrayList<String> dets = new ArrayList<String>();
+        dets.add("Kept the monies and bought a bunch of pizza");
+        this.experience2 = new ClubExperience(UUID.randomUUID(), "ACM", dets, Date.fromString("05/09/2020"), Date.fromString("05/10/2020"), "Treasurer");
+        this.experience = new ClubExperience("President of ACM");
+    }
+	
+	@AfterEach
+	public void tearDown() {
+
+	}
+
+    @Test
+    public void testToString() {
+		String str1 = "President of ACM";
+        String str2 = this.experience.toString();
+		assertEquals(str1, str2);
+    }
+
+    @Test
+    public void testToStringLong() {
+        String str2 = this.experience2.toString();
+        String str1 = "ACM (05/09/20 - 05/10/20)\nTreasurer\n *  Kept the monies and bought a bunch of pizza";
+        assertEquals(str1, str2);
+    }
+
+    @Test
+    public void testToJSON() {   
+        String str1 = this.experience.toJSON();
+        String str2 = "{\"title\": \"President of ACM\"}";
+        assertEquals(str1, str2);
+    }
+
+	@Test
+	public void testFromJSON() {
+		ClubExperience experience3 = ClubExperience.fromJSON("{\"id\":\"741690ea-c168-4211-9b75-bcba8c6fd1ea\",\"title\":\"Association for Computing Machinery UofSC Branch\",\"details\":[\"Served as club president\"],\"startDate\":\"08/05/20\",\"endDate\":\"12/20/20\",\"role\":\"Club President\"}");
+        assertEquals(experience3.getTitle(), "Association for Computing Machinery UofSC Branch");
+	}
+    
+	
+	//assertEquals(val1,val2)
+	//assertFalse(val)
+	//assertTrue(val)
+	//assertSame(val1,val2)
+	//assertNotSame(val1,val2)
+	//assertNull(val)
+	//assertNotNull(val)
+}
--- test/ListingTest.java
+++ test/ListingTest.java
@@ -39,10 +39,16 @@ class ListingTest {
 	}
 
     @Test
-    public void testToString() {
-		Listing listing2 = Listing.fromJSON("{\"id\":\"f2c88aaa-dd37-46e4-abaf-919856ab5c4c\",\"employerId\":\"01f78a8e-21a0-40cc-8b03-6155cb92986f\",\"payRate\":18.75,\"location\":\"Greenville, South Carolina\",\"title\":\"Software Engineering Intern\",\"description\":[],\"startDate\":\"05/01/20\",\"endDate\":\"08/20/20\",\"siteLink\":\"https: //www.publix.com/\",\"skills\":[ \"Java\",\"Linux\",\"CSS\"],\"applicants\":[\"aae52f6d-08a1-4775-a2ac-ff7f631cd686\",\"6ab93632-f1c8-441f-bf57-e63384ae3c58\"]}");
-		String str1 = listing1.toString(true);
-		String str2 = listing2.toString(true);
+    public void testToStringFalse() {
+		String str1 = "Publix Supermarkets Inc - Software Engineering Intern\n********************\n\nLocation: Greenville, South Carolina\nPay Rate: 18.75\nStart Date: 05/01/20\nEnd Date: 08/20/20\n\nRequired Skills:\nJava\nLinux\nCSS\n\nhttps: //www.publix.com/";
+		String str2 = listing1.toString(false);
+		assertEquals(str1, str2);
+    }
+
+	@Test
+    public void testToStringTrue() {
+		String str1 = "Publix Supermarkets Inc - Software Engineering Intern\n********************\nApplicants:\naae52f6d-08a1-4775-a2ac-ff7f631cd686\n6ab93632-f1c8-441f-bf57-e63384ae3c58\n\n\nLocation: Greenville, South Carolina\nPay Rate: 18.75\nStart Date: 05/01/20\nEnd Date: 08/20/20\n\nRequired Skills:\nJava\nLinux\nCSS\n\nhttps: //www.publix.com/";
+		String str2 = listing1.toString(true);
 		assertEquals(str1, str2);
     }
 
