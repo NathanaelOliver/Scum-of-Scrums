0c8deb1c6e63f1cc28621a833e9aa0e47f1aba4f
wihobbs <hobbsman58@gmail.com>
2021-11-14 23:20:08 -0500

    more tests

--- test/EmployerTest.java
+++ test/EmployerTest.java
@@ -45,6 +45,18 @@ class EmployerTest {
 	}
 
 	@Test
+	public void testBigConstructor() {
+		// Employer employer2 = new Employer(UUID.)
+
+	}
+
+	@Test
+	public void testShortConstructor() {
+		Employer employer2 = new Employer("lbnl", "gobears12!", "Lawrence Berkeley National Laboratory");
+		assertEquals(employer2.getTitle)
+	}
+
+	@Test
 	public void testToJSON() {
 		assertEquals(employer1.toJSON(), "{\"id\":\"01f78a8e-21a0-40cc-8b03-6155cb92986f\",\"username\":\"publix\",\"password\":\"p9o08h80n\",\"userType\":\"employer\",\"isVerified\":true,\"title\":\"Publix Supermarkets Inc\",\"description\":[],\"listings\":[ {\"id\":\"f2c88aaa-dd37-46e4-abaf-919856ab5c4c\",\"employerId\":\"01f78a8e-21a0-40cc-8b03-6155cb92986f\",\"payRate\":18.75,\"location\":\"Greenville, South Carolina\",\"title\":\"Software Engineering Intern\",\"description\":[],\"startDate\":\"05/01/20\",\"endDate\":\"08/20/20\",\"siteLink\":\"https: //www.publix.com/\",\"skills\":[ \"Java\",\"Linux\",\"CSS\"],\"applicants\":[\"aae52f6d-08a1-4775-a2ac-ff7f631cd686\",\"6ab93632-f1c8-441f-bf57-e63384ae3c58\"]}]}");
 	}
